143fc1e2d61108901e02b08a0afc5c171a42e322
mayankagl <mayank.garg@adglobal360.com>
2026-04-27 23:25:01 +0530

    feat: implement AppService with hello world endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Pipeline RUN Successfully form master branch!';
+    return 'Final Check for Pipeline!';
   }
 }
